e77b58317d6af9ecaa115b103a7bf1e2184bd719
Michael Xie <65536583+xmx6665@users.noreply.github.com>
2021-12-10 11:46:26 +0800

    Add files via upload

--- impl/antiPolandExp.cpp
+++ impl/antiPolandExp.cpp
@@ -109,7 +109,7 @@ void pop_and_print(stack<number_stack_elem> &number_stack, stack<return_token> &
 		if (x2.is_function) {
 			// TODO 添加函数内容
 		} else {
-			fprintf(output, "%%%d = icmp ne i32 0, ", register_num++);
+			fprintf(output, "%%%d = icmp ne i32 0, ", register_num);
 			print_number_stack_elem(x2);
 			stringstream stream2;
 			stream2 << register_num++;
